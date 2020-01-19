3992a606a5a9f6a9f77b197961bd3121cb626166
oswind <51972981+oswind@users.noreply.github.com>
2020-01-19 18:58:14 +0300

    Add files via upload

---
+++ blog/models/Post.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "posts".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $categories
+ * @property string|null $content
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'posts';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['content'], 'string'],
+            [['title', 'categories'], 'string', 'max' => 32],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'categories' => 'Categories',
+            'content' => 'Content',
+        ];
+    }
+}

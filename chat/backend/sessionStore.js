47d6f3617a65c41e638b2584745c9e9194e5b3a5
oswind <51972981+oswind@users.noreply.github.com>
2020-01-19 18:49:07 +0300

    Add files via upload

---
+++ chat/backend/sessionStore.js
@@ -0,0 +1,8 @@
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const sessionStore = new MongoStore({
+    url: 'mongodb+srv://user:user@cluster0-lwvqu.mongodb.net/test?retryWrites=true&w=majority',
+});
+
+module.exports = sessionStore;
